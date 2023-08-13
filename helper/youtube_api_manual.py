aa489a9014e4570d56a6ea2630090b9d4975cda5
tasyasoni <tasyasoni1@gmail.com>
2023-08-13 20:20:09 +0300

    "final files"

--- helper/youtube_api_manual.py
+++ helper/youtube_api_manual.py
@@ -8,7 +8,7 @@ import isodate
 
 
 # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
-api_key: str = os.getenv('YT_API_KEY')
+api_key: str = os.getenv("YT_API_KEY")
 
 # создать специальный объект для работы с API
 youtube = build('youtube', 'v3', developerKey=api_key)
@@ -17,6 +17,7 @@ youtube = build('youtube', 'v3', developerKey=api_key)
 def printj(dict_to_print: dict) -> None:
     """Выводит словарь в json-подобном удобном формате с отступами"""
     print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
+
 
 
 '''
--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,9 @@
+import json
+import os
+from googleapiclient.discovery import build
+
+api_key: str = os.getenv("YT_API_KEY")
+youtube = build('youtube', 'v3', developerKey=api_key)
 
 
 class Channel:
@@ -5,8 +11,12 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
-    def print_info(self) -> None:
+
+    def print_info(self):
         """Выводит в консоль информацию о канале."""
-        pass
+        channel_info = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        print(json.dumps(channel_info, indent=4, ensure_ascii=False))
+
+
